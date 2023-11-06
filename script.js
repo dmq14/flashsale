f0b5fdeabfeeb8ba4c8f46f97f930669df2f1303
dmq14 <dangminhquang1401@gmail.com>
2023-11-06 15:34:26 +0700

    banner, carousel, product flash sale

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-html
+function scrollContainerLeft() {
+    document.getElementById('productListContainer').scrollBy({
+        left: -250,
+        behavior: 'smooth'
+    });
+    document.getElementById('productListContainer2').scrollBy({
+        left: -250,
+        behavior: 'smooth'
+    });
+}
+
+function scrollContainerRight() {
+    document.getElementById('productListContainer').scrollBy({
+        left: 250,
+        behavior: 'smooth'
+    });
+    document.getElementById('productListContainer2').scrollBy({
+        left: 250,
+        behavior: 'smooth'
+    });
+}
