16549a4a63f7092639d8acf32d2d342b51129d1e
dmq14 <dangminhquang1401@gmail.com>
2023-11-07 12:30:19 +0700

    update

--- script.js
+++ script.js
@@ -19,3 +19,475 @@ function scrollContainerRight() {
         behavior: 'smooth'
     });
 }
+function scrolllaptopmoidellLeft() {
+    document.getElementById('scrolllaptopmoidell').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoidellRight() {
+    document.getElementById('scrolllaptopmoidell').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoihpLeft() {
+    document.getElementById('scrolllaptopmoihp').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoihpRight() {
+    document.getElementById('scrolllaptopmoihp').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoilenovoLeft() {
+    document.getElementById('scrolllaptopmoilenovo').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoilenovoRight() {
+    document.getElementById('scrolllaptopmoilenovo').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptopmoiasusLeft() {
+    document.getElementById('scrolllaptopmoiasus').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoiasusRight() {
+    document.getElementById('scrolllaptopmoiasus').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptopmoiacerLeft() {
+    document.getElementById('scrolllaptopmoiacer').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoiacerRight() {
+    document.getElementById('scrolllaptopmoiacer').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptopmoimsiLeft() {
+    document.getElementById('scrolllaptopmoimsi').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoimsiRight() {
+    document.getElementById('scrolllaptopmoimsi').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptopmoimicrosoftLeft() {
+    document.getElementById('scrolllaptopmoimicrosoft').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopmoimicrosoftRight() {
+    document.getElementById('scrolllaptopmoimicrosoft').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptopgamingLeft() {
+    document.getElementById('scrolllaptopgaming').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopgamingRight() {
+    document.getElementById('scrolllaptopgaming').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptopdellcuLeft() {
+    document.getElementById('scrolllaptopdellcu').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopdellcuRight() {
+    document.getElementById('scrolllaptopdellcu').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptophpcuLeft() {
+    document.getElementById('scrolllaptophpcu').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptophpcuRight() {
+    document.getElementById('scrolllaptophpcu').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptoplenovocuLeft() {
+    document.getElementById('scrolllaptoplenovocu').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptoplenovocuRight() {
+    document.getElementById('scrolllaptoplenovocu').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllaptopasuscuLeft() {
+    document.getElementById('scrolllaptopasuscu').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllaptopasuscuRight() {
+    document.getElementById('scrolllaptopasuscu').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollpcgamingLeft() {
+    document.getElementById('scrollpcgaming').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollpcgamingRight() {
+    document.getElementById('scrollpcgaming').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollpcdohoaLeft() {
+    document.getElementById('scrollpcdohoa').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollpcdohoaRight() {
+    document.getElementById('scrollpcdohoa').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollpcvanphongLeft() {
+    document.getElementById('scrollpcvanphong').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollpcvanphongRight() {
+    document.getElementById('scrollpcvanphong').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollpcallinoneLeft() {
+    document.getElementById('scrollpcallinone').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollpcallinoneRight() {
+    document.getElementById('scrollpcallinone').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollpcminiLeft() {
+    document.getElementById('scrollpcmini').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollpcminiRight() {
+    document.getElementById('scrollpcmini').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolldellmtLeft() {
+    document.getElementById('scrolldellmt').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolldellmtRight() {
+    document.getElementById('scrolldellmt').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollhpmtLeft() {
+    document.getElementById('scrollhpmt').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollhpmtRight() {
+    document.getElementById('scrollhpmt').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllenovomtLeft() {
+    document.getElementById('scrolllenovomt').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllenovomtRight() {
+    document.getElementById('scrolllenovomt').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollacermtLeft() {
+    document.getElementById('scrollacermt').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollacermtRight() {
+    document.getElementById('scrollacermt').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollsamsungmhLeft() {
+    document.getElementById('scrollsamsungmh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollsamsungmhRight() {
+    document.getElementById('scrollsamsungmh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollmsimhLeft() {
+    document.getElementById('scrollmsimh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollmsimhRight() {
+    document.getElementById('scrollmsimh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollmsimhLeft() {
+    document.getElementById('scrollmsimh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollmsimhRight() {
+    document.getElementById('scrollmsimh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolldellmhLeft() {
+    document.getElementById('scrolldellmh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolldellmhRight() {
+    document.getElementById('scrolldellmh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollhpmhLeft() {
+    document.getElementById('scrollhpmh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollhpmhRight() {
+    document.getElementById('scrollhpmh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllenovomhLeft() {
+    document.getElementById('scrolllenovomh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllenovomhRight() {
+    document.getElementById('scrolllenovomh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollviewsonicmhLeft() {
+    document.getElementById('scrollviewsonicmh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollviewsonicmhRight() {
+    document.getElementById('scrollviewsonicmh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollacermhLeft() {
+    document.getElementById('scrollacermh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollacermhRight() {
+    document.getElementById('scrollacermh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolllgmhLeft() {
+    document.getElementById('scrolllgmh').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolllgmhRight() {
+    document.getElementById('scrolllgmh').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrolltaingheLeft() {
+    document.getElementById('scrolltainghe').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrolltaingheRight() {
+    document.getElementById('scrolltainghe').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollsaclaptopLeft() {
+    document.getElementById('scrollsaclaptop').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollsaclaptopRight() {
+    document.getElementById('scrollsaclaptop').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollchuotLeft() {
+    document.getElementById('scrollchuot').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollchuotRight() {
+    document.getElementById('scrollchuot').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollbanphimLeft() {
+    document.getElementById('scrollbanphim').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollbanphimRight() {
+    document.getElementById('scrollbanphim').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollocungLeft() {
+    document.getElementById('scrollocung').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollocungRight() {
+    document.getElementById('scrollocung').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
+function scrollramLeft() {
+    document.getElementById('scrollram').scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+}
+
+function scrollramRight() {
+    document.getElementById('scrollram').scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+}
