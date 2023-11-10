fe392d8b823ea4b37cb35b346c9f3cc1bc7c8997
dmq14 <dangminhquang1401@gmail.com>
2023-11-10 14:27:57 +0700

    update

--- script.js
+++ script.js
@@ -4,61 +4,63 @@ const desktopBtns = document.querySelectorAll(".menu-btn .btn-check");
 const sections = document.querySelectorAll("section");
 
 const handleScroll = (index) => {
-    sections[index].scrollIntoView({ behavior: "smooth", block: "start" });
-    lastChecked = index;
+  sections[index].scrollIntoView({ behavior: "smooth" });
+  lastChecked = index;
 };
 
 mobileBtns.forEach((btn, index) => {
-    btn.addEventListener("click", () => {
-        handleScroll(index);
-    });
+  btn.addEventListener("click", () => {
+    handleScroll(index);
+  });
 });
 
 desktopBtns.forEach((btn, index) => {
-    btn.addEventListener("click", () => {
-        handleScroll(index);
-    });
+  btn.addEventListener("click", () => {
+    handleScroll(index);
+  });
 });
 
 window.addEventListener("scroll", () => {
-    sections.forEach((section, index) => {
-        const top = section.getBoundingClientRect().top;
-        if (top >= 0 && top <= window.innerHeight) {
-            mobileBtns[lastChecked].checked = false;
-            mobileBtns[index].checked = true;
-            desktopBtns[lastChecked].checked = false;
-            desktopBtns[index].checked = true;
-            lastChecked = index;
-        }
-    });
+  sections.forEach((section, index) => {
+    const top = section.getBoundingClientRect().top;
+    if (top >= 0 && top <= window.innerHeight) {
+      mobileBtns[lastChecked].checked = false;
+      mobileBtns[index].checked = true;
+      desktopBtns[lastChecked].checked = false;
+      desktopBtns[index].checked = true;
+      lastChecked = index;
+    }
+  });
 });
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('section');
     const radioBtns = document.querySelectorAll('.btn-check');
-
+  
     function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
+      const rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
     }
-
+  
     function handleScroll() {
-        sections.forEach((section, index) => {
-            if (isInViewport(section)) {
-                radioBtns.forEach((btn) => {
-                    btn.checked = false;
-                });
-                radioBtns[index].checked = true;
-            }
-        });
+      sections.forEach((section, index) => {
+        if (isInViewport(section)) {
+          radioBtns.forEach((btn) => {
+            btn.checked = false;
+          });
+          radioBtns[index].checked = true;
+        }
+      });
     }
-
+  
     window.addEventListener('scroll', handleScroll);
-});
+    window.addEventListener('touchmove', handleScroll);
+  });
+  
 
 function scrollContainerLeft() {
     document.getElementById('productListContainer').scrollBy({
